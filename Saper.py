bdd9dc16fee2dcac26e6da5ab61075b0d14e5dae
Tochilin Yury <tochilin.yury@gmail.com>
2020-05-03 19:05:29 +0300

    saper

---
+++ Saper.py
@@ -0,0 +1,22 @@
+string, stolb, mins = (int(i) for i in input().split())
+pole = [[0 for i in range(stolb)] for j in range(string)]
+
+for i in range(mins):
+    row, col = (int(i) - 1 for i in input().split())
+    pole[row][col] = '*'
+for i in range(string):
+    for j in range(stolb):
+        if pole[i][j]==0:
+            for di in range(-1,2):
+                for dj in range(-1,2):
+                    smest_str = i + di
+                    smest_stolb = j + dj
+                    if 0 <= smest_stolb < stolb and 0<= smest_str<string and pole[smest_str][smest_stolb] == '*':
+                        pole[i][j] +=1
+for i in range(string):
+    for j in range(stolb):
+        if pole[i][j]==0:
+            print('.',end='')
+        else:
+            print(pole[i][j],end='')
+    print()
