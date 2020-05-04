56f9718a886841a1bdf7447459956008fcecba53
Tochilin Yury <tochilin.yury@gmail.com>
2020-05-04 19:35:37 +0300

    Modify file

--- matrixI/matrix.py
+++ matrixI/matrix.py
@@ -8,10 +8,13 @@ if matrix_in != []:
     kol_stolb = int(len(matrix_in[0]))
 matrix_out = []
 for i in range(kol_string):
-        matrix_out.append([0]*kol_stolb)
+    matrix_out.append([0] * kol_stolb)
 # if kol_string == 1:
 for i in range(kol_string):
     for j in range(kol_stolb):
-            matrix_out[i][j] = matrix_in[i][j - kol_stolb + 1] + matrix_in[i][j - 1] + matrix_in[i - kol_string + 1][j] + matrix_in[i - 1][j]
+        matrix_out[i][j] = matrix_in[i][j - kol_stolb + 1] + \
+                           matrix_in[i][j - 1] + \
+                           matrix_in[i - kol_string + 1][j] + \
+                           matrix_in[i - 1][j]
 for i in matrix_out:
     print(*i)
